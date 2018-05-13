4c053116002b8336d6836d393618179cd2f3d716
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2018-05-12 22:32:52 -0400

    raw implmentation of the nav (to fix) and of the html rendering (styling fails)

--- Section.js
+++ Section.js
@@ -5,19 +5,27 @@ export default class Section extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+            "clickStatus": "false"
+        }
     }
 
     render() {
-		let components = []
-		this.props.sections.map((section) => {
-			components.push(<Button title={ section } />)
-		})
-
-		return(
-			<View>
-				<Text> { this.props.area } </Text>
-				{ components }
-			</View>
-		)
+        if (this.state.clickStatus == "false") {
+            return(
+                <View>
+                    <Button title={ this.props.name } onPress={ () => this.setState({"clickStatus": "true" }) } >
+                    </Button>
+                </View>
+            )
+        } else {
+            return(
+                <View>
+                    <Button title={ this.props.name } onPress={ () => this.setState({"clickStatus": "false" }) } >
+                    </Button>
+                    { this.props.children }
+                </View>
+            )
+        }
     }
 }
